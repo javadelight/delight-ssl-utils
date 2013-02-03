01103fb577b683e5ffcb8c3a5c934ad05daffdb6
mxro <mxro@nowhere.com>
2013-02-03 04:05:21 +0000

    allowing to configure ssl certificates for rest servers

--- src/main/java/mx/sslutils/MxSslUtils.java
+++ src/main/java/mx/sslutils/MxSslUtils.java
@@ -13,45 +13,47 @@ import javax.net.ssl.SSLContext;
 import mx.sslutils.internal.SslContextFactory;
 
 public class MxSslUtils {
-	
-	private static Map<SslKeyStoreData,SSLContext> cache;
-	
-	static {
-		cache = new IdentityHashMap<SslKeyStoreData,SSLContext>();
-	}
-	
-	/**
-	 * A generic certificate for a linnk.it node.
-	 * 
-	 * @return
-	 */
-	public static SslKeyStoreData getLinnkItServerCertificate() {
-		return new NxUncertifiedSslKeyStore();
-	}
-	
-	public static SSLContext createContextForCertificate(final SslKeyStoreData keyStoreData) {
-		if (cache.containsKey(keyStoreData)) {
-			return cache.get(keyStoreData);
-		}
-		
-		final SSLContext newContext = SslContextFactory.getServerContext(keyStoreData);
-		cache.put(keyStoreData, newContext);
-		
-		return newContext;
-	}
 
-	public static SSLContext getClientContext() {
-		SSLContext clientContext = null;
-		try {
-			clientContext = SSLContext.getInstance(SslContextFactory.PROTOCOL);
-			clientContext.init(null, SslTrustManagerFactory.getTrustManagers(),
-					null);
-		} catch (final Exception e) {
-			throw new Error("Failed to initialize the client-side SSLContext",
-					e);
-		}
-		return clientContext;
-	}
+    private static Map<SslKeyStoreData, SSLContext> cache;
 
-	
+    static {
+        cache = new IdentityHashMap<SslKeyStoreData, SSLContext>();
+    }
+
+    /**
+     * A generic certificate for a linnk.it node.
+     * 
+     * @return
+     */
+    public static SslKeyStoreData getLinnkItServerCertificate() {
+        return new NxUncertifiedSslKeyStore();
+    }
+
+    public static SSLContext createContextForCertificate(
+            final SslKeyStoreData keyStoreData) {
+        if (cache.containsKey(keyStoreData)) {
+            return cache.get(keyStoreData);
+        }
+
+        final SSLContext newContext = SslContextFactory
+                .getServerContext(keyStoreData);
+
+        cache.put(keyStoreData, newContext);
+
+        return newContext;
+    }
+
+    public static SSLContext getClientContext() {
+        SSLContext clientContext = null;
+        try {
+            clientContext = SSLContext.getInstance(SslContextFactory.PROTOCOL);
+            clientContext.init(null, SslTrustManagerFactory.getTrustManagers(),
+                    null);
+        } catch (final Exception e) {
+            throw new Error("Failed to initialize the client-side SSLContext",
+                    e);
+        }
+        return clientContext;
+    }
+
 }
